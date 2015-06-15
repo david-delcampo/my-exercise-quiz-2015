ef38206e6daa830189496d2786d522ac641cc4cf
David <david@cincoinfo.com>
2015-06-15 23:09:13 +0200

    Crear pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,6 +14,24 @@ exports.load = function(req, res, next, quizId) {
   ).catch(function(error) { next(error); });
 };
   
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build( // crea objeto Quiz
+    { pregunta: "Pregunta", respuesta: "Respuesta" }
+  );
+  
+  res.render('quizes/new', { quiz: quiz });
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build(req.body.quiz);
+  
+  // guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save( {fields: ["pregunta","respuesta"]} ).then(function() {
+    res.redirect('/quizes');
+  })
+};
 
 // GET /quizes
 exports.index = function(req, res) {  
