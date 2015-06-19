a443c273f12324fee0d450b8d4840842f5992430
David <david@cincoinfo.com>
2015-06-19 21:02:28 +0200

    Se añade el campo tema al modelo Quiz

--- models/quiz.js
+++ models/quiz.js
@@ -11,6 +11,11 @@ module.exports = function(sequelize, DataTypes) {
 	    type: DataTypes.STRING,
 	    validate: {
 	      notEmpty: {msg: "-> Falta Respuesta"}}
-	  }	      
+	  },
+	  tema: {
+	    type: DataTypes.STRING,
+	    validate: {
+	      notEmpty: {msg: "-> Falta Tema"}}
+	  }		  
   });
 }
